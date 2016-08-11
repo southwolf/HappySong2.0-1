a5e77a0e8fc0af8ddeecc00fbacd33e7d1eb1490
ljmob <ljmob@ljmob.com>
2016-08-11 10:46:04 +0800

    modify sms

--- app/api/v1/users_api.rb
+++ app/api/v1/users_api.rb
@@ -15,7 +15,7 @@ module V1
         if user.blank?
           user = User.create(:phone => phone)
         end
-        if user.deliver_fake_sms
+        if user.deliver
           present :message, "成功"
         else
           error!("失败", 500)
