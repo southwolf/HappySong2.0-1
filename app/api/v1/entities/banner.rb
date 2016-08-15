0586a7a7f0a942b218957b663e0ba256006cc160
ljmob <ljmob@ljmob.com>
2016-08-15 17:04:48 +0800

    fix bugs

--- app/api/v1/entities/banner.rb
+++ app/api/v1/entities/banner.rb
@@ -1,6 +1,6 @@
 module Entities
   class Banner < Grape::Entity
     expose :id, :text
-    expose :cover_img {| object| ENV['QINIUPREFIX']+object.cover_img}
+    expose (:cover_img) {|object| ENV['QINIUPREFIX']+object.cover_img}
   end
 end
