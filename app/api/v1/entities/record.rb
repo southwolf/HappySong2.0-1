41abdd607f2b3b8dee49d8bfe2f5d85e18ebfc75
ljmob <ljmob@ljmob.com>
2016-08-16 10:06:46 +0800

    fix

--- app/api/v1/entities/record.rb
+++ app/api/v1/entities/record.rb
@@ -1,6 +1,6 @@
 module Entities
   class Record < Grape::Entity
-    expose :id, :feeling, :style, :created_at
+    expose :id, :feeling, :style,:is_demo, :is_hot,:view_count, :created_at
     expose (:file_url) {|object| ENV['QINIUPREFIX']+object.file_url}
     expose :user,     using: Entities::User
     expose :music,    using: Entities::Music
