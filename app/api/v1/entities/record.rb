3a0818b86189607c6f03cbc0e9cf9942c5f1c2b1
ljmob <ljmob@ljmob.com>
2016-08-29 14:42:00 +0800

    fix

--- app/api/v1/entities/record.rb
+++ app/api/v1/entities/record.rb
@@ -2,18 +2,6 @@ module Entities
   class Record < Grape::Entity
     expose :id, :feeling, :style, :is_demo, :is_hot, :view_count, :comments_count, :likes_count, :created_at
     expose (:file_url) {|object| ENV['QINIUPREFIX']+object.file_url}
-    expose (:width) do |object|
-      uri = URI(ENV['QINIUPREFIX']+object.file_url+'?imageInfo')
-      res = ::Net::HTTP.get_response(uri)
-      info = ::ActiveSupport::JSON.decode res.body
-      info["width"]
-    end
-    expose (:height) do |object|
-      uri = URI(ENV['QINIUPREFIX']+object.file_url+'?imageInfo')
-      res = ::Net::HTTP.get_response(uri)
-      info = ::ActiveSupport::JSON.decode res.body
-      info["height"]
-    end
     expose :user,     using: Entities::User
     expose :music,    using: Entities::Music
     expose :article,  using: Entities::Article
