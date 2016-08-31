455b2a966e79474a601323913ed19cca8b49b382
ljmob <ljmob@ljmob.com>
2016-08-31 18:44:20 +0800

    fix

--- app/api/v1/entities/album.rb
+++ app/api/v1/entities/album.rb
@@ -3,17 +3,17 @@ module Entities
     expose :id, :created_at
     expose (:file_url) { |object| ENV['QINIUPREFIX']+object.file_url}
 
-    expose (:width) do |object|
-      uri = URI(ENV['QINIUPREFIX']+object.file_url+'?imageInfo')
-      res = ::Net::HTTP.get_response(uri)
-      info = ::ActiveSupport::JSON.decode res.body
-      info["width"]
-    end
-    expose (:height) do |object|
-      uri = URI(ENV['QINIUPREFIX']+object.file_url+'?imageInfo')
-      res = ::Net::HTTP.get_response(uri)
-      info = ::ActiveSupport::JSON.decode res.body
-      info["height"]
-    end
+    # expose (:width) do |object|
+    #   uri = URI(ENV['QINIUPREFIX']+object.file_url+'?imageInfo')
+    #   res = ::Net::HTTP.get_response(uri)
+    #   info = ::ActiveSupport::JSON.decode res.body
+    #   info["width"]
+    # end
+    # expose (:height) do |object|
+    #   uri = URI(ENV['QINIUPREFIX']+object.file_url+'?imageInfo')
+    #   res = ::Net::HTTP.get_response(uri)
+    #   info = ::ActiveSupport::JSON.decode res.body
+    #   info["height"]
+    # end
   end
 end
--- app/api/v1/entities/banner.rb
+++ app/api/v1/entities/banner.rb
@@ -2,17 +2,17 @@ module Entities
   class Banner < Grape::Entity
     expose :id, :text, :target, :target_id
     expose (:cover_img) {|object| ENV['QINIUPREFIX']+object.cover_img}
-    expose (:width) do |object|
-      uri = URI(ENV['QINIUPREFIX']+object.cover_img+'?imageInfo')
-      res = ::Net::HTTP.get_response(uri)
-      info = ::ActiveSupport::JSON.decode res.body
-      info["width"]
-    end
-    expose (:height) do |object|
-      uri = URI(ENV['QINIUPREFIX']+object.cover_img+'?imageInfo')
-      res = ::Net::HTTP.get_response(uri)
-      info = ::ActiveSupport::JSON.decode res.body
-      info["height"]
-    end
+    # expose (:width) do |object|
+    #   uri = URI(ENV['QINIUPREFIX']+object.cover_img+'?imageInfo')
+    #   res = ::Net::HTTP.get_response(uri)
+    #   info = ::ActiveSupport::JSON.decode res.body
+    #   info["width"]
+    # end
+    # expose (:height) do |object|
+    #   uri = URI(ENV['QINIUPREFIX']+object.cover_img+'?imageInfo')
+    #   res = ::Net::HTTP.get_response(uri)
+    #   info = ::ActiveSupport::JSON.decode res.body
+    #   info["height"]
+    # end
   end
 end
