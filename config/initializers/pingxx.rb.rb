10453da0417d9d4bdf000342814f0bc88aa462b9
ljmob <ljmob@ljmob.com>
2016-09-02 17:38:37 +0800

    fix

--- config/initializers/pingxx.rb.rb
+++ config/initializers/pingxx.rb.rb
@@ -1,4 +1,4 @@
 require "pingpp"
-Pingpp.api_key = "sk_test_SKmjH4SGW5uHiTqjPKS0aT04"
+Pingpp.api_key = "sk_live_a9qjX5i1u5CO9i1Wv5KiX5uL"
 Pingpp.private_key_path = File.dirname(__FILE__)+"/pingpp_rsa_private_key.pem"
 Pingpp.pub_key_path = File.dirname(__FILE__)+"/pingpp_rsa_public_key.pem"
