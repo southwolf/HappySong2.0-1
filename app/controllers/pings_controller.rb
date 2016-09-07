7ff3e841087d026a47e365c8b7aa4286d8ede699
ljmob <ljmob@ljmob.com>
2016-09-07 18:22:20 +0800

    fix

--- app/controllers/pings_controller.rb
+++ app/controllers/pings_controller.rb
@@ -14,42 +14,42 @@ class PingsController < ApplicationController
       if Pingpp::Webhook.verify?(request)
         status =  400
         response_body = ''
-        begin
-          event = params
-          puts  "begin"
-          if event['type'].nil?
-            puts "fuck"
-          elsif event['type'] == 'charge.succeeded'
-            # 年费
-            #
-            puts "成功"
-            puts  event['data']['object']['amount']
-            if event['data']['object']['amount'] == 10000
-              puts "年费"
-              order_no = event['data']['object']['order_no']
-              bill = Bill.find_by(order_no: order_no)
-              bill.update(:complete => true)
-              #完成支付
-              bill.complete
-              #月费
-            elsif event['data']['object']['amount'] == 100
-              puts "月费"
-              order_no = event['data']['object']['order_no']
-              bill = Bill.find_by(order_no: order_no)
-              bill.update(:complete => true)
-              bill.complete
-            else
-              puts "支付失败"
-            end
-            status = 200
-            response_body = 'OK'
-
+        # begin
+        event = params
+        puts  "begin"
+        if event['type'].nil?
+          puts "fuck"
+        elsif event['type'] == 'charge.succeeded'
+          # 年费
+          #
+          puts "成功"
+          puts  event['data']['object']['amount']
+          if event['data']['object']['amount'] == 10000
+            puts "年费"
+            order_no = event['data']['object']['order_no']
+            bill = Bill.find_by(order_no: order_no)
+            bill.update(:complete => true)
+            #完成支付
+            bill.complete
+            #月费
+          elsif event['data']['object']['amount'] == 100
+            puts "月费"
+            order_no = event['data']['object']['order_no']
+            bill = Bill.find_by(order_no: order_no)
+            bill.update(:complete => true)
+            bill.complete
           else
-            response_body = '未知类型'
+            puts "支付失败"
           end
-        rescue JSON::ParserError
-          response_body = 'JSON解析失败'
+          status = 200
+          response_body = 'OK'
+
+        else
+          response_body = '未知类型'
         end
+        # rescue JSON::ParserError
+          # response_body = 'JSON解析失败'
+        # end
 
         response.body = response_body
         request['Content-Type'] = 'text/plain; charset=utf-8'
