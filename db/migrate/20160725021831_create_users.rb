56b07df90946f18852f83096c9a372292db25bd2
ljmob <ljmob@ljmob.com>
2016-09-12 12:55:10 +0800

    fix

--- db/migrate/20160725021831_create_users.rb
+++ db/migrate/20160725021831_create_users.rb
@@ -1,18 +1,18 @@
-class CreateUsers < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-      t.string  :phone
-      t.string  :name
-      t.string  :uid
-      t.string  :avatar,   default: 'happysong_logo.jpg'
-      t.string  :sex
-      t.string  :age
-      t.string  :desc
-      t.boolean :vip,      default: false
-      t.boolean :is_first, default: true
-      t.string :code
-
-      t.timestamps
-    end
-  end
-end
+# class CreateUsers < ActiveRecord::Migration
+#   def change
+#     create_table :users do |t|
+#       t.string  :phone
+#       t.string  :name
+#       t.string  :uid
+#       t.string  :avatar,   default: 'happysong_logo.jpg'
+#       t.string  :sex
+#       t.string  :age
+#       t.string  :desc
+#       t.boolean :vip,      default: false
+#       t.boolean :is_first, default: true
+#       t.string :code
+# 
+#       t.timestamps
+#     end
+#   end
+# end
