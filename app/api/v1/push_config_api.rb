8975332d6a10efb5007df05f7017786cafd0ace4
ljmob <ljmob@ljmob.com>
2016-09-26 15:25:42 +0800

    fix

--- app/api/v1/push_config_api.rb
+++ app/api/v1/push_config_api.rb
@@ -17,16 +17,45 @@ module V1
         when 3
           current_user.add_push_action(PushAction.find_by(action:'like'))
         end
-
-        present :message, "更新成功"
+        result = {
+          message: "更新成功"
+        }
+        present :message, result
       end
+
+
+
+
+      desc "开启接受指定消息"
+      params do
+        requires :token,  type: String, desc: "用户登录令牌"
+        requires :notify, type: Integer, desc: "消息类型自己传ID [新粉丝: 1, 评论: 2, 喜欢: 3]"
+      end
+      post '/open' do
+        authenticate!
+        notify = params[:notify]
+        case notify
+        when 1
+          current_user.remove_push_action(PushAction.find_by(action: 'follow'))
+        when 2
+          current_user.remove_push_action(PushAction.find_by(action:'comment'),PushAction.find_by(action: 'reply'))
+        when 3
+          current_user.remove_push_action(PushAction.find_by(action:'like'))
+        end
+        result = {
+          message: "更新成功"
+        }
+        present :message, result
+        
+      end
+
 
 
       desc "检查是否关闭指定消息"
       params do
         requires :token,  type: String, desc: "用户登录令牌"
       end
-      post '/check' do
+      get '/check' do
         authenticate!
         result = {
           follow: false,
@@ -48,26 +77,6 @@ module V1
         end
         present result
       end
-
-
-      desc "开启接受指定消息"
-      params do
-        requires :token,  type: String, desc: "用户登录令牌"
-        requires :notify, type: Integer, desc: "消息类型自己传ID [新粉丝: 1, 评论: 2, 喜欢: 3]"
-      end
-      post '/open' do
-        authenticate!
-        notify = params[:notify]
-        case notify
-        when 1
-          current_user.remove_push_action(PushAction.find_by(action: 'follow'))
-        when 2
-          current_user.remove_push_action(PushAction.find_by(action:'comment'),PushAction.find_by(action: 'reply'))
-        when 3
-          current_user.remove_push_action(PushAction.find_by(action:'like'))
-        end
-      end
-      present :message, "更新成功"
 
     end
   end
