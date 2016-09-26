161b882636f66eb2d1f30af331df8aeeba9b9be5
ljmob <ljmob@ljmob.com>
2016-09-26 18:41:19 +0800

    fix

--- app/api/v1/helpers/record_helpers.rb
+++ app/api/v1/helpers/record_helpers.rb
@@ -3,7 +3,7 @@ module V1
   module RecordHelpers
     extend Grape::API::Helpers
     def update_hot
-      if Time.now.day = Time.now.end_of_week
+      # if Time.now.day = Time.now.end_of_week
         records = Record.find_by_sql("SELECT a.id, count(b.num) FROM records as a join views as b on a.id = b.view_record_id
                             WHERE (b.`created_at` BETWEEN '#{Time.now.end_of_day - 7.day}' AND '#{Time.now.end_of_day}') GROUP BY(a.id) Limit 10")
         Record.all.each do |record|
@@ -13,7 +13,7 @@ module V1
         records.each do |record|
           Record.find(record.id).update(:is_hot => true)
         end
-      end
+      # end
     end
   end
 end
