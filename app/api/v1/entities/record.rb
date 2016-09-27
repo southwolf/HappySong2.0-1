434ce80386c0bf3bb2b0de05e1243387060ef2d1
ljmob <ljmob@ljmob.com>
2016-09-27 09:41:22 +0800

    fix

--- app/api/v1/entities/record.rb
+++ app/api/v1/entities/record.rb
@@ -2,23 +2,11 @@ module Entities
   class Record < Grape::Entity
     expose :id, :feeling, :style, :is_demo, :is_hot, :view_count, :comments_count, :likes_count, :created_at, :is_public
     expose (:file_url) {|object| ENV['QINIUPREFIX']+object.file_url}
-    expose :user,     using: Entities::User
-    expose :music,    using: Entities::Music
-    expose :article,  using: Entities::Article
+    # expose :user,     using: Entities::User
+    # expose :music,    using: Entities::Music
+    # expose :article,  using: Entities::Article
     expose (:share_url) {|object| ENV['SHARERECORD']+"/share_record/#{object.id}"}
     # expose :comments, using: Entities::CommentWithReply
-    # expose (:liked?) do |object, option|
-    #   current_user = option[:current_user]
-    #   if current_user.present?
-    #     if object.like_users.include? current_user
-    #       true
-    #     else
-    #       false
-    #     end
-    #   else
-    #     false
-    #   end
-    # end
   end
   class SimpleRecord < Grape::Entity
     expose :id, :feeling, :style, :is_demo, :is_hot, :view_count, :comments_count, :likes_count, :created_at,:is_public
