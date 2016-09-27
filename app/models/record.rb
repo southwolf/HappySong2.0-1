36a2e9771f9d2570012e1065c51853e433d62d5b
ljmob <ljmob@ljmob.com>
2016-09-27 09:24:12 +0800

    fix

--- app/models/record.rb
+++ app/models/record.rb
@@ -19,7 +19,7 @@ class Record < ActiveRecord::Base
 
   after_commit :async_create_record_notify, on: :create
 
-  scope :public_record, -> { where(:public => true)}
+  scope :public_record, -> { where(:is_public => true)}
   def async_create_record_notify
     NotifyRecordJob.perform_later(id)
   end
