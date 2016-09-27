c25fc4220db2c9cf34b6ce9ff8cedc04fec223e4
ljmob <ljmob@ljmob.com>
2016-09-27 09:44:51 +0800

    fix

--- app/api/v1/entities/music.rb
+++ app/api/v1/entities/music.rb
@@ -10,20 +10,7 @@ module Entities
     end
     expose (:file_url) { |object| ENV['QINIUPREFIX']+ object.file_url}
     expose (:cover_img){ |object| ENV['QINIUPREFIX']+ object.cover_img}
-    
-    # expose (:width) do |object|
-    #   uri = URI(ENV['QINIUPREFIX']+object.cover_img+'?imageInfo')
-    #   res = ::Net::HTTP.get_response(uri)
-    #   info = ::ActiveSupport::JSON.decode res.body
-    #   info["width"]
-    # end
-
-    # expose (:height) do |object|
-    #   uri = URI(ENV['QINIUPREFIX']+object.cover_img+'?imageInfo')
-    #   res = ::Net::HTTP.get_response(uri)
-    #   info = ::ActiveSupport::JSON.decode res.body
-    #   info["width"]
-    # end
+  
   end
 
   class MusicType < Grape::Entity
