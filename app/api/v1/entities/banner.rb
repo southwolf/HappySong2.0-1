c7d820245a2ef9403b9f4215927d977269e3615b
ljmob <ljmob@ljmob.com>
2016-09-30 16:31:09 +0800

    fix

--- app/api/v1/entities/banner.rb
+++ app/api/v1/entities/banner.rb
@@ -1,6 +1,13 @@
 module Entities
   class Banner < Grape::Entity
-    expose :id, :text, :targetable_type, :targetable_id, :link_url
+    expose :id, :text, :targetable_type, :targetable_id
+    expose (:link_url) do |object|
+      if object.link_url.nil?
+        ""
+      else
+        object.link_url
+      end
+    end
     expose (:cover_img) {|object| ENV['QINIUPREFIX']+object.cover_img}
   end
 end
