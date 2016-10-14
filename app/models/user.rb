716731bc281c12784c4e2892a48f84086c8580a9
ljmob <ljmob@ljmob.com>
2016-10-14 17:17:48 +0800

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -147,7 +147,7 @@ class User < ActiveRecord::Base
   def classmates
     grade_team_class = self.grade_team_class
     return nil if grade_team_class.nil?
-    return nil if grade_team_class.student.nil?
+    return nil if grade_team_class.students.nil?
     grade_team_class.students.reject { |student| student == self  }
   end
 
