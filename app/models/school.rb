62c75b76e21d74bab18e3c9d112845720efd698f
ljmob <ljmob@ljmob.com>
2016-10-19 11:02:43 +0800

    fix

--- app/models/school.rb
+++ app/models/school.rb
@@ -5,7 +5,7 @@ class School < ActiveRecord::Base
   has_many :grade_team_classes
 
   has_many :channel_schools,  dependent: :destroy
-  has_many :channel_users, :through => :channel_schools
+  has_many :channel_users,    :through => :channel_schools
 
   after_create :update_info
 
