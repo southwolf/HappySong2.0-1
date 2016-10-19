24db0868d6029af7b608e35bce1d5d6ace2e4b64
ljmob <ljmob@ljmob.com>
2016-10-19 16:46:28 +0800

    fix

--- app/api/v1/entities/tag.rb
+++ app/api/v1/entities/tag.rb
@@ -2,7 +2,7 @@ module Entities
   class Tag < Grape::Entity
     expose :id, :name
     expose (:cover_img) do |object|
-      object.cover_img
+      ENV['QINIUPREFIX'] + object.cover_img
     end
   end
   class ShowTag < Tag
