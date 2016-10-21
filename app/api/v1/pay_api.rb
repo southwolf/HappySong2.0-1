18ecf2cb505d4773d3a1de05e54f98b8f8ec9617
ljmob <ljmob@ljmob.com>
2016-10-21 11:52:47 +0800

    fix

--- app/api/v1/pay_api.rb
+++ app/api/v1/pay_api.rb
@@ -5,22 +5,6 @@ module V1
 
     namespace :pay do
 
-      desc "测试"
-      post '/fuck' do
-        res = Pingpp::Charge.create(
-          :order_no  => "1231231231234214324",
-          :app       => { :id => 'app_yT48q5PWfLyL8qvj'},
-          :channel   => "wx",
-          :amount    => 100,
-          :client_ip => "101.228.149.134",
-          :currency  => "cny",
-          :subject   => "sdasd",
-          :body      => "sadasdas"
-        )
-        puts res
-        present res
-
-      end
       desc "支付接口"
       params do
         requires :channel,        type: String,  desc: "渠道类型"
@@ -63,17 +47,6 @@ module V1
             :subject   => subject,
             :body      => body
           )
-
-          # charge = Pingpp::Charge.create(
-          #   :order_no  => "1231231231234214324",
-          #   :app       => { :id => 'app_yT48q5PWfLyL8qvj'},
-          #   :channel   => "wx",
-          #   :amount    => 100,
-          #   :client_ip => "101.228.149.134",
-          #   :currency  => "cny",
-          #   :subject   => "sdasd",
-          #   :body      => "sadasdas"
-          # )
           present charge
         rescue Pingpp::PingppError => error
             error!({error: error}, 500)
