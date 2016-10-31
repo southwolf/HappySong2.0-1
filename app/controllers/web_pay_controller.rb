eda920082310250b69565ee72c34d08c73bbff8c
ljmob <ljmob@ljmob.com>
2016-10-31 14:09:03 +0800

    fix

--- app/controllers/web_pay_controller.rb
+++ app/controllers/web_pay_controller.rb
@@ -2,6 +2,7 @@ class WebPayController < ApplicationController
 
   def pay
     @user = User.find_by_auth_token(params[:token])
+    #redirect_to "http://www.rabbitpre.com/m/7IRInVRAm"
   end
   def success
 
