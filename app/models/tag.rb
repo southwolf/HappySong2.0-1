40b5b5dbc31b39d5ee1548f1878edb9dfc67adf0
ljmob <ljmob@ljmob.com>
2016-11-02 17:55:29 +0800

    fix

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ActiveRecord::Base
-  has_many :taggings
+  has_many :taggings, dependent: :destroy
   has_many :dynamics, through: :taggings
-  
+
   # 推荐的标签
   scope :recommend, ->{ where(:recommend => true) }
 end
