c9570baa92f66a2f3a52feb876f6269240efc31d
ljmob <ljmob@ljmob.com>
2016-11-03 12:39:40 +0800

    fix

--- app/api/v1/tags_api.rb
+++ app/api/v1/tags_api.rb
@@ -14,7 +14,7 @@ module V1
       end
       get '/search_tags' do
         tag_name = params[:tag_name]
-        tags = Tag.where(:recommend => false).where("name like ?", "#{tag_name}%").order(:created_at => :desc)
+        tags = Tag.where(:recommend => false).where("name like ?", "#{tag_name}%").order(:tag_heat => :desc)
         present tags, with: ::Entities::Tag
       end
     end
