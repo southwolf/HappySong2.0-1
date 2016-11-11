0c62840d7ff80bec3e866464300f994690695708
ljmob <ljmob@ljmob.com>
2016-11-11 14:36:20 +0800

    fix

--- app/controllers/web_pay_controller.rb
+++ app/controllers/web_pay_controller.rb
@@ -1,17 +1,17 @@
 class WebPayController < ApplicationController
 
   def pay
-    @user = User.find_by_auth_token(params[:token])
-    # redirect_to "http://www.rabbitpre.com/m/7IRInVRAm"
+    # @user = User.find_by_auth_token(params[:token])
+    redirect_to "http://www.rabbitpre.com/m/7IRInVRAm"
     #
-    render layout: false
+    # render layout: false
   end
 
   def other_pay
-    @user = User.find_by_auth_token(params[:token])
-      # redirect_to "http://www.rabbitpre.com/m/7IRInVRAm"
+    # @user = User.find_by_auth_token(params[:token])
+    redirect_to "http://www.rabbitpre.com/m/7IRInVRAm"
     #
-    render layout: false
+    # render layout: false
   end
   def success
 
