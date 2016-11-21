3ce0a488393652e2e2db1f146961761c6f8e78fc
ZengWeimao <so.zengtao@gmail.com>
2016-11-21 15:23:42 +0800

    ADD: add staging env

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,6 +3,6 @@ puts 'use staging'
 set :domain, '121.41.104.215'
 set :port, '22'
 set :user, 'deploy'
-set :deploy_to, '/opt/rails/happysong2/'
+set :deploy_to, '/opt/rails/happysong2'
 set :rails_env, 'rails-5'
 set :keep_releases, 1
