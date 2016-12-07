5c7ba03eebd168718baf0733f5ac200447f8c6d1
w-zengtao <so.zengtao@gmail.com>
2016-12-07 10:26:58 +0800

    ADD: Student 通过班级码加入一个班级

--- app/models/user/student.rb
+++ app/models/user/student.rb
@@ -1,6 +1,13 @@
 class Student < User
 
+  # associations
+  has_many :class_students, foreign_key: :student_id, class_name: 'ClassStudent'
+  has_many :org_classes, through: :class_students # 学生可以加入多个班级
+
   # instance methods
-  def join_class(class_name) # 加入班级
+  def join_class(class_code) # 加入班级
+    org_class = Org::Class.find_by(code: class_code)
+    class_student = class_students.build(org_class: org_class)
+    class_student.save
   end
 end
